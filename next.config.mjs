26616f05ee0615fd93b60fd8782963489c181337
kaliblip-inc <kaliblip@gmail.com>
2026-09-01 20:54:28 +0000

    add auth support for network host

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,21 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  allowedDevOrigins: [
+    '172.20.10.7',
+    '172.20.10.7:3000',
+    'localhost:3000',
+    '127.0.0.1:3000',
+    '*.local',
+  ],
   experimental: {
     serverActions: {
       bodySizeLimit: '200mb',
+      allowedOrigins: [
+        '172.20.10.7:3000',
+        'localhost:3000',
+        '127.0.0.1:3000',
+      ],
     },
   },
 }
